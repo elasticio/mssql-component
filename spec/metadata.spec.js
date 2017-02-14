dc28ef1cd13adda15f10f9dea193361b560fbefa
Renat Zubairov <renat.zubairov@gmail.com>
2017-02-14 13:24:21 +0100

    Added first test (now with mocha). Mocha is the best testing framework in the world, really #great

---
+++ spec/metadata.spec.js
@@ -0,0 +1,88 @@
+'use strict';
+const expect = require('chai').expect;
+const insert = require('../lib/actions/insert');
+
+describe('Metadata test', () => {
+  const cfg = {
+    query: 'INSERT INTO Test2.dbo.Tweets (Lang, Retweeted, Favorited, "Text", id, CreatedAt, Username, ScreenName) '
+    + 'VALUES (@lang:string, @retweeted:boolean, @favorited:boolean, @text:string, @id:bigint, @created_at:date, @username:string, @screenname:string)'
+  };
+
+
+  it('should generate metadata', (done) => {
+    insert.getMetaModel(cfg, (err, result) => {
+      expect(err).to.be.null;
+      expect(result).to.deep.equal({
+          "in": {
+            "properties": {
+              "created_at": {
+                "type": "string"
+              },
+              "favorited": {
+                "type": "string"
+              },
+              "id": {
+                "type": "number"
+              },
+              "lang": {
+                "type": "string"
+              },
+              "retweeted": {
+                "type": "string"
+              },
+              "screenname": {
+                "type": "string"
+              },
+              "text": {
+                "type": "string"
+              },
+              "username": {
+                "type": "string"
+              }
+            },
+            "type": "object"
+          },
+          "out": {}
+        }
+      );
+      done();
+    });
+  });
+
+  it('should not fail on empty query', (done) => {
+    insert.getMetaModel({}, (err, result) => {
+      expect(err).to.be.null;
+      expect(result).to.deep.equal({
+          "in": {
+            "properties": {},
+            "type": "object"
+          },
+          "out": {}
+        }
+      );
+      done();
+    });
+  });
+
+  it('should not handle duplicate fields', (done) => {
+    insert.getMetaModel({
+      query: '@foo:string @foo:string @bar:date'
+    }, (err, result) => {
+      expect(err).to.be.null;
+      expect(result).to.deep.equal({
+          "in": {
+            "type": "object",
+            "properties": {
+              "foo": {type: 'string'},
+              "bar": {type: 'string'}
+            }
+          },
+          "out": {}
+        }
+      );
+      done();
+    });
+  });
+
+
+});

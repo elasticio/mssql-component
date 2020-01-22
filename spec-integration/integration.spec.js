9de79ead1f096333438238e141c8ab709ebe9f7d
uaArsen <arsenzhd@gmail.com>
2020-01-22 15:48:29 +0200

    Fix tests

--- spec-integration/integration.spec.js
+++ spec-integration/integration.spec.js
@@ -1,31 +1,10 @@
-'use strict';
 const expect = require('chai').expect;
 const insert = require('../lib/actions/insert');
 const select = require('../lib/actions/select');
 const { messages } = require('elasticio-node');
 const EventEmitter = require('events');
+const sinon = require('sinon');
 const logger = require('@elastic.io/component-logger')();
-
-class TestEmitter extends EventEmitter {
-
-    constructor(done) {
-        super();
-        this.data = [];
-        this.end = 0;
-        this.error = [];
-
-        this.on('data', (value) => this.data.push(value));
-        this.on('error', (value) => {
-            this.error.push(value);
-            console.error(value.stack || value);
-        });
-        this.on('end', () => {
-            this.end++;
-            done();
-        });
-    }
-
-}
 
 describe('Integration test', () => {
 
@@ -34,6 +13,9 @@ describe('Integration test', () => {
         if (!process.env.MSSQL_PASSWORD) {throw new Error('Please set MSSQL_PASSWORD env variable to proceed');}
         if (!process.env.MSSQL_SERVER) {throw new Error('Please set MSSQL_SERVER env variable to proceed');}
         if (!process.env.MSSQL_DATABASE) {throw new Error('Please set MSSQL_DATABASE env variable to proceed');}
+    });
+    after(() => {
+        process.exit();
     });
 
     describe('for INSERT', () => {
@@ -55,7 +37,7 @@ describe('Integration test', () => {
         before(() => insert.init(cfg));
 
         it('should insert data', () => {
-            const emitter = new TestEmitter();
+            const emitter = sinon.stub();
             const msg = {
                 body: {
                     lang: 'en',
@@ -73,11 +55,7 @@ describe('Integration test', () => {
                 logger
             }, msg).then((result) => {
                 expect(result).deep.equal(msg);
-                expect(emitter.data.length).to.equal(0);
-                // promises, no need to emit end
-                expect(emitter.end).to.equal(0);
-                // No error
-                expect(emitter.error.length).to.equal(0);
+                expect(emitter.called).to.be.false;
             });
         });
     });
@@ -98,17 +76,19 @@ describe('Integration test', () => {
         before(() => select.init(cfg));
 
         it('should select data', (done) => {
-            const emitter = new TestEmitter(() => {
-                expect(emitter.error.length).to.equal(0);
-                expect(emitter.data.length).to.equal(10);
-                expect(emitter.end).to.equal(1);
+            const emitter = sinon.stub();
+            emitter.withArgs('end').callsFake(() => {
+                expect(emitter.callCount).to.equal(12);
+                expect(emitter.args[0][0]).to.equal('data');
+                expect(emitter.args[10][0]).to.equal('snapshot');
+                expect(emitter.args[11][0]).to.equal('end');
                 done();
             });
             const msg = messages.newMessageWithBody({
                 query: 'select * from Test2.dbo.Tweets ORDER BY id OFFSET 10 ROWS FETCH NEXT 10 ROWS ONLY;'
             });
             select.process.call({
-                emit: emitter.emit,
+                emit: emitter,
                 logger
             }, msg, cfg).catch(err => done(err));
         });
@@ -131,15 +111,17 @@ describe('Integration test', () => {
         before(() => select.init(cfg));
 
         it('should select data', (done) => {
-            const emitter = new TestEmitter(() => {
-                expect(emitter.error.length).to.equal(0);
-                expect(emitter.data.length).to.equal(10);
-                expect(emitter.end).to.equal(1);
+            const emitter = sinon.stub();
+            emitter.withArgs('end').callsFake(() => {
+                expect(emitter.callCount).to.equal(12);
+                expect(emitter.args[0][0]).to.equal('data');
+                expect(emitter.args[10][0]).to.equal('snapshot');
+                expect(emitter.args[11][0]).to.equal('end');
                 done();
             });
             const msg = messages.newMessageWithBody({});
             select.process.call({
-                emit: emitter.emit,
+                emit: emitter,
                 logger
             }, msg, cfg).catch(err => done(err));
         });
@@ -162,9 +144,10 @@ describe('Integration test', () => {
         before(() => select.init(cfg));
 
         it('should insert data', (done) => {
-            const emitter = new TestEmitter(() => {
-                expect(emitter.error.length).to.equal(0);
-                expect(emitter.end).to.equal(1);
+            const emitter = sinon.stub();
+            emitter.withArgs('end').callsFake(() => {
+                expect(emitter.args[0][0]).to.equal('data');
+                expect(emitter.args[emitter.args.length - 1][0]).to.equal('end');
                 done();
             });
             const msg = {
@@ -173,10 +156,9 @@ describe('Integration test', () => {
                 }
             };
             select.process.call({
-                emit: emitter.emit,
+                emit: emitter,
                 logger
             }, msg, cfg, {}).catch(err => done(err));
         });
     });
-
 });

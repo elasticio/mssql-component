9820f9e08503107e75550715811090986f38f464
Olha Virolainen <olga.shukel@gmail.com>
2020-11-18 12:27:23 +0200

    Annual audit of the component code to check if it exposes a sensitive data in the logs

---
+++ spec-integration/verifyCredentials.spec.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const { expect } = require('chai');
+const logger = require('@elastic.io/component-logger')();
+const verifyCredentials = require('../verifyCredentials');
+
+describe('Integration test verify credentials', () => {
+  if (fs.existsSync('.env')) {
+    // eslint-disable-next-line global-require
+    require('dotenv').config();
+  }
+  before(() => {
+    if (!process.env.MSSQL_USERNAME) { throw new Error('Please set MSSQL_USERNAME env variable to proceed'); }
+    if (!process.env.MSSQL_PASSWORD) { throw new Error('Please set MSSQL_PASSWORD env variable to proceed'); }
+    if (!process.env.MSSQL_SERVER) { throw new Error('Please set MSSQL_SERVER env variable to proceed'); }
+    if (!process.env.MSSQL_DATABASE) { throw new Error('Please set MSSQL_DATABASE env variable to proceed'); }
+  });
+  after(() => {
+    process.exit();
+  });
+
+  const cfg = {
+    username: process.env.MSSQL_USERNAME,
+    password: process.env.MSSQL_PASSWORD,
+    server: process.env.MSSQL_SERVER,
+    port: process.env.MSSQL_PORT,
+    instance: process.env.MSSQL_INSTANCE,
+    database: process.env.MSSQL_DATABASE,
+    domain: process.env.MSSQL_DOMAIN,
+    encrypt: process.env.MSSQL_ENCRYPT,
+  };
+
+  it('should insert data', (done) => {
+    verifyCredentials.call({ logger }, cfg, (err, result) => {
+      if (err) {
+        done(err);
+      }
+      expect(result).deep.equal({ verified: true });
+      done();
+    });
+  });
+});
--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,12 +1,12 @@
-
 const co = require('co');
 const sql = require('co-mssql');
 
 // This function will be called by the platform to verify credentials
 module.exports = function verifyCredentials(credentials, cb) {
-  console.log('Credentials passed for verification %j', credentials);
+  const self = this;
+  self.logger.info('Starting credentials verification');
   co(function* () {
-    console.log('Connecting to the database');
+    self.logger.info('Connecting to the database');
     const uri = `mssql://${
       encodeURIComponent(credentials.username)
     }:${
@@ -21,11 +21,11 @@ module.exports = function verifyCredentials(credentials, cb) {
       : `?encrypt=${credentials.encrypt}`}`;
     const connection = new sql.Connection(uri);
     yield connection.connect();
-    console.log('Verification completed successfully');
+    self.logger.info('Verification completed successfully');
     yield connection.close();
     cb(null, { verified: true });
   }).catch((err) => {
-    console.log('Error occurred', err.stack || err);
+    self.logger.info('Error occurred, credentials are not valid');
     cb(err, { verified: false });
   });
 };

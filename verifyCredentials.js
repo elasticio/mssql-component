fd72e113972361cb4c0eb3cadbff0d45743dbf71
Olha Virolainen <olga.virolainen@elastic.io>
2020-11-20 11:36:02 +0200

    Annual npm vulnerabilities audit (#21)
    Annual npm vulnerabilities audit

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,31 +1,31 @@
-'use strict';
 const co = require('co');
 const sql = require('co-mssql');
 
 // This function will be called by the platform to verify credentials
 module.exports = function verifyCredentials(credentials, cb) {
-  console.log('Credentials passed for verification %j', credentials);
-  co(function*() {
-    console.log('Connecting to the database');
-    var uri = 'mssql://'
-      + encodeURIComponent(credentials.username)
-      + ':'
-      + encodeURIComponent(credentials.password)
-      + '@'
-      + credentials.server
-      + ((credentials.port) ? ':' + credentials.port : '')
-      + ((credentials.instance) ? '/' + credentials.instance : '')
-      + '/'
-      + credentials.database
-      + ((credentials.domain) ? '?domain=' + credentials.domain + '&encrypt=' + credentials.encrypt
-                              : '?encrypt=' + credentials.encrypt);
-    var connection = new sql.Connection(uri);
+  const self = this;
+  self.logger.info('Starting credentials verification');
+  co(function* () {
+    self.logger.info('Connecting to the database');
+    const uri = `mssql://${
+      encodeURIComponent(credentials.username)
+    }:${
+      encodeURIComponent(credentials.password)
+    }@${
+      credentials.server
+    }${(credentials.port) ? `:${credentials.port}` : ''
+    }${(credentials.instance) ? `/${credentials.instance}` : ''
+    }/${
+      credentials.database
+    }${(credentials.domain) ? `?domain=${credentials.domain}&encrypt=${credentials.encrypt}`
+      : `?encrypt=${credentials.encrypt}`}`;
+    const connection = new sql.Connection(uri);
     yield connection.connect();
-    console.log('Verification completed successfully');
+    self.logger.info('Verification completed successfully');
     yield connection.close();
-    cb(null, {verified: true});
-  }).catch(err => {
-    console.log('Error occurred', err.stack || err);
-    cb(err , {verified: false});
+    cb(null, { verified: true });
+  }).catch((err) => {
+    self.logger.info('Error occurred, credentials are not valid');
+    cb(err, { verified: false });
   });
 };
